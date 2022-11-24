44ae76fa7288df4830c7d292e6d8268ea557c966
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2022-11-24 23:28:41 +0300

    edited

---
+++ my.sh
@@ -0,0 +1,1 @@
+gcc -Wall -Werror -Wextra -pedantic -g src/main.c src/init_instance.c src/draw_functions.c src/map_functions.c src/game_functions.c src/paint_functions.c src/calculation_functions.c src/intersect_functions.c -lm $(sdl2-config --cflags) $(sdl2-config --libs) -lSDL2_ttf
